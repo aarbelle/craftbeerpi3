249925092aa0f44fc5119fea473e5673afca837d
BrewChef <30447284+BrewChef@users.noreply.github.com>
2020-05-14 08:51:35 +0200

    Create fixit.sh

---
+++ fixit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install python-pip
+sudo pip install flask
+sudo pip install flask_socketio
+sudo pip install flask_classy
+sudo pip install PyYAML
+sudo pip install GitPython
+sudo pip install requests
+sudo pip install gitdb2==2.0.5
+
